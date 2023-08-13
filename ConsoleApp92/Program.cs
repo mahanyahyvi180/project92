4fdcd623c4b0e5d93c5fa3a12fbbbe3f332498c1
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-13 19:50:19 +0330

    Add files via upload

---
+++ ConsoleApp92/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new BasePlusCommissionEmployee("Bob", "Lewis", "333-33-333", 5000.00M, .04M, 300.00M);
+
+            Console.WriteLine("Employee information obtained by properties and methods:\n");
+            Console.WriteLine($"First name is{employee.FirstName}");
+            Console.WriteLine($"Last name is{employee.LastName}");
+            Console.WriteLine($"Social security number is{employee.SocialSecurityNumber}");
+            Console.WriteLine($"Gross sales are {employee.GrossSales:C}");
+            Console.WriteLine($"Commission rate is{employee.CommissionRate:F2}");
+            Console.WriteLine($"Earnings are {employee.Earings:C}");
+            Console.WriteLine($"Base salary is{employee.BaseSalary:C}");
+
+            employee.BaseSalary = 1000.00M;
+
+            Console.WriteLine("\nUpdated employee information obtained by ToString:\n");
+            Console.WriteLine(employee);
+            Console.WriteLine($"earnings:{employee.Earings:C}");
+            Console.ReadLine();
+
+        }
+    }
+}
